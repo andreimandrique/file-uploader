d7fa2d1f97e818aa1e0fb14fa8f04aa7b1df7e83
Andrei <andreimandrique3@gmail.com>
2025-03-01 20:19:05 +0800

    move to commonjs

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(express.urlencoded({ extended: true }));
+
+const indexRouter = require("./routes/indexRouter");
+
+app.get("/", indexRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
